b9474ea7ebf67c507046068035194f7d5ea77caf
AdiKsOnDev <adilalizade13@gmail.com>
2026-01-06 14:24:07 +0400

    refactor(theme): Force light theme by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <div className="flex h-screen overflow-hidden">
